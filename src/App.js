8b518e460dcbdd3333241c90eca701c4019c2a15
Elena Giacomazzi <elenagiacomazzi@Elenas-MBP.fritz.box>
2020-12-04 18:00:38 +0100

    bissl Ordnung hier

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './data/logo.svg';
+import './CSS/App.css';
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hallo Franzi!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LOOOOOOOS Learn React
         </a>
       </header>
     </div>
