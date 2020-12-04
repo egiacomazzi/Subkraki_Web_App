5e7bfbb8ddd78ac4c2a905c621eaababe81c0293
Elena Giacomazzi <elenagiacomazzi@Elenas-MBP.fritz.box>
2020-12-04 18:11:58 +0100

    bissl Ordnung

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hallo Franzi!
+          Hallo Franzi! Hallo Leon!
         </p>
         <a
           className="App-link"
