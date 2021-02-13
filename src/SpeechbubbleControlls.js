e11449cf602df70fe06c29eba7b239966c8cc97a
Elena Sophie Giacomazzi <elena-sophie.giacomazzi@stud.uni-bamberg.de>
2021-02-13 09:44:31 +0100

    Arrow as button there

--- src/SpeechbubbleControlls.js
+++ src/SpeechbubbleControlls.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './CSS/SpeechbubbleControlls.css';
 import PropTypes from 'prop-types';
 import Speechbubble from './Speechbubble.js';
+import Arrow from './Arrow.js';
 
 class SpeechbubbleControlls extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stepNumber: 0,
+    };
+  }
   render() {
     return (
-      <div>
+      <div className="SpeechbubbleControls">
         <Speechbubble text={this.props.text} />
+        <Arrow class="rightway" />
       </div>
     );
   }
