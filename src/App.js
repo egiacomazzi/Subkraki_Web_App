426983ecfc8f49d0468924062f6449e9824023df
Elena Sophie Giacomazzi <elena-sophie.giacomazzi@stud.uni-bamberg.de>
2021-02-15 17:28:09 +0100

    Imports fix after merge

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './CSS/App.css';
-import Speechbubble from './Speechbubble';
-import Subkraki from './Subkraki.js';
-import SubtractionPanel from './SubtractionPanel/SubtractionPanel.js'
-
+import SubtractionPanel from './SubtractionPanel/SubtractionPanel.js';
+import Welcome from './Welcome.js';
 
 function App() {
   return (
     <div className="App">
       <Welcome />
-      <SubtractionPanel subtrahend="777" minuend="456" digits="3"/>
+      <SubtractionPanel subtrahend="777" minuend="456" digits="3" />
     </div>
   );
 }
