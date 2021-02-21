e26d5576e8efae6a81d874e70698197a1ca405b6
Elena Sophie Giacomazzi <elena-sophie.giacomazzi@stud.uni-bamberg.de>
2021-02-21 22:36:19 +0100

    navigation after welcome to calculation works

--- src/App.js
+++ src/App.js
@@ -3,13 +3,55 @@ import './CSS/App.css';
 import SubtractionPanel from './SubtractionPanel/SubtractionPanel.js';
 import Welcome from './Welcome.js';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <Welcome />
+//       <SubtractionPanel subtrahend="777" minuend="456" digits="3" />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+export default function App() {
   return (
-    <div className="App">
-      <Welcome />
-      <SubtractionPanel subtrahend="777" minuend="456" digits="3" />
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/">Nothing</Link>
+          </li>
+          <li>
+            <Link to="/welcome">Welcome</Link>
+          </li>
+          <li>
+            <Link to="/substactionpanel">Math</Link>
+          </li>
+        </ul>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/substactionpanel">
+            <SubtractionPanel
+              subtrahend="777"
+              minuend="456"
+              digits="3"
+            />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
--- src/Welcome.js
+++ src/Welcome.js
@@ -2,34 +2,18 @@ import React from 'react';
 //import './CSS/Welcome.css';
 import Subkraki from './Subkraki.js';
 import SpeechbubbleControlls from './SpeechbubbleControlls.js';
+//import history from './history.js';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 
 class Welcome extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       introTextIndex: 0,
+      // nextFunc: this.nextText(),
     };
-  }
-  nextText() {
-    if (this.state.introTextIndex == 7) {
-      return;
-    }
-    this.setState({
-      introTextIndex: this.state.introTextIndex + 1,
-    });
-  }
-
-  lastText() {
-    if (this.state.introTextIndex == 0) {
-      return;
-    }
-    this.setState({
-      introTextIndex: this.state.introTextIndex - 1,
-    });
-  }
-
-  render() {
-    const text = {
+    this.text = {
       intro: [
         'Hallo! Ich bin Subkraki und möchte mit dir das Subtrahieren üben.',
         'Ich erkläre dir nun, was du alles hier in der Unterwasserwelt machen kannst.',
@@ -41,19 +25,67 @@ class Welcome extends React.Component {
         'Viel Spaß beim Rechnen!',
       ],
     };
-    return (
-      <div className="welcome">
-        <SpeechbubbleControlls
-          text={text.intro[this.state.introTextIndex]}
-          nextText={() => this.nextText()}
-          lastText={() => this.lastText()}
-          beginning={this.state.introTextIndex === 0 ? true : false}
-        />
+  }
 
-        <Subkraki size="big" />
-      </div>
-    );
+  lastText() {
+    if (this.state.introTextIndex == 0) {
+      return;
+    }
+    this.setState({
+      introTextIndex: this.state.introTextIndex - 1,
+    });
+  }
+
+  nextText() {
+    console.log(this.state.introTextIndex);
+    if (this.state.introTextIndex == 7) {
+      // history.push('/substactionpanel');
+
+      return;
+    }
+    this.setState({
+      introTextIndex: this.state.introTextIndex + 1,
+    });
+  }
+
+  endWelcome() {
+    this.props.history.push('/substactionpanel');
+  }
+
+  render() {
+    if (this.state.introTextIndex === 7) {
+      return (
+        <div className="welcome">
+          <SpeechbubbleControlls
+            text={this.text.intro[this.state.introTextIndex]}
+            nextText={() => this.endWelcome()}
+            lastText={() => this.lastText()}
+            beginning={this.state.introTextIndex === 0 ? true : false}
+          />
+
+          <Subkraki size="big" />
+        </div>
+      );
+    } else {
+      return (
+        <div className="welcome">
+          <SpeechbubbleControlls
+            text={this.text.intro[this.state.introTextIndex]}
+            nextText={() => this.nextText()}
+            lastText={() => this.lastText()}
+            beginning={this.state.introTextIndex === 0 ? true : false}
+          />
+
+          <Subkraki size="big" />
+        </div>
+      );
+    }
   }
 }
 
-export default Welcome;
+export default withRouter(Welcome);
+Welcome.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
