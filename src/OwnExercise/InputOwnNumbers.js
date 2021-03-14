6f97fcad38720033a133c9ff6f54d165526bf741
Elena Sophie Giacomazzi <elena-sophie.giacomazzi@stud.uni-bamberg.de>
2021-03-14 12:29:52 +0100

    ErrorMessages wird angezeigt

--- src/OwnExercise/InputOwnNumbers.js
+++ src/OwnExercise/InputOwnNumbers.js
@@ -3,6 +3,14 @@ import React from 'react';
 //import PropTypes from 'prop-types';
 
 class InputOwnNumbers extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showError: false,
+    };
+    this.errorText = null;
+    this.returnSubMintoSub = this.returnSubMintoSub.bind(this);
+  }
   returnSubMintoSub() {
     var minuend = document.getElementById('obereZahl').value;
     var subtrahend = document.getElementById('untereZahl').value;
@@ -12,37 +20,73 @@ class InputOwnNumbers extends React.Component {
       subtrahend == '' ||
       subtrahend == null
     ) {
+      this.errorText = 'Bitte gib die Zahlen ein.';
+      this.setState({
+        showError: true,
+      });
       console.log('input bitte');
-    }
-    if (Number(minuend) < Number(subtrahend)) {
+    } else if (Number(minuend) < Number(subtrahend)) {
+      this.errorText =
+        'Die obere Zahl muss größer sein als die untere. Probiere es nochmal.';
       console.log('Oben muss größer als unten sein');
+      this.setState({
+        showError: true,
+      });
+    } else {
+      console.log(minuend, subtrahend);
     }
-    console.log(minuend, subtrahend);
   }
   render() {
-    return (
-      <div className="inputOwnEx">
-        <div className="obereZahlText">Obere Zahl:</div>
-        <input
-          type="number"
-          className="obereZahlInput"
-          id="obereZahl"
-        ></input>
-        <div className="untereZahlText">Untere Zahl:</div>
-        <input
-          type="number"
-          className="untereZahlInput"
-          id="untereZahl"
-        ></input>
+    if (!this.state.showError) {
+      return (
+        <div className="inputOwnEx">
+          <div className="obereZahlText">Obere Zahl:</div>
+          <input
+            type="number"
+            className="obereZahlInput"
+            id="obereZahl"
+          ></input>
+          <div className="untereZahlText">Untere Zahl:</div>
+          <input
+            type="number"
+            className="untereZahlInput"
+            id="untereZahl"
+          ></input>
 
-        <button
-          className="eigeneAufgabeSubmit"
-          onClick={this.returnSubMintoSub}
-        >
-          Los
-        </button>
-      </div>
-    );
+          <button
+            className="eigeneAufgabeSubmit"
+            onClick={this.returnSubMintoSub}
+          >
+            Los
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="inputOwnEx">
+          <div className="obereZahlText">Obere Zahl:</div>
+          <input
+            type="number"
+            className="obereZahlInput"
+            id="obereZahl"
+          ></input>
+          <div className="untereZahlText">Untere Zahl:</div>
+          <input
+            type="number"
+            className="untereZahlInput"
+            id="untereZahl"
+          ></input>
+          <p className="errorText">{this.errorText}</p>
+
+          <button
+            className="eigeneAufgabeSubmit"
+            onClick={this.returnSubMintoSub}
+          >
+            Los
+          </button>
+        </div>
+      );
+    }
   }
 }
 
